43c223db594d73c40f759825bf4ce97e31994307
Kush Patel <kushpatel2354@gmail.com>
2025-10-06 13:19:40 +0530

    Fourth Commit

--- src/main.jsx
+++ src/main.jsx
@@ -5,4 +5,8 @@ import App from './App';
 import './styles/layout.css';
 import "./index.css";
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />);
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
