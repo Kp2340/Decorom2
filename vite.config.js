90fe3270932f9c7ff40e079180087339e882ccbd
Kush Patel <kushpatel2354@gmail.com>
2025-10-26 22:40:33 +0530

    vite.config.js changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 import { ViteImageOptimizer } from 'vite-plugin-image-optimizer';
 
 export default defineConfig({
-  base: '/Decorom2/',
+  base: '/',
   plugins: [
     react(),
     ViteImageOptimizer({
